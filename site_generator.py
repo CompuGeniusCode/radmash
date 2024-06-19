81b147e0a012ebb42e73206c0d1547b7379fbada
CompuGenius <contact@cgprograms.com>
2024-06-19 19:01:18 -0400

    Added parsha order

--- site_generator.py
+++ site_generator.py
@@ -1,22 +1,28 @@
 import os
 import re
 
-from titlecase import titlecase
 from jinja2 import Environment, FileSystemLoader
+from titlecase import titlecase
 
 PDF_DIR = 'divrei_torah/maamarei_mordechai'
-OUTPUT_DIR = 'output'
 TEMPLATE_DIR = 'templates'
 
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-
 env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
-
 
 # for filename in os.listdir(PDF_DIR):
 #     if filename.endswith('.pdf'):
 #         new_filename = filename.lower().removeprefix("_")
 #         os.rename(os.path.join(PDF_DIR, filename), os.path.join(PDF_DIR, new_filename))
+
+
+parsha_order = ["Bereishis", "Noach", "Lech Lecha", "Vayera", "Chayei Sarah", "Toldos", "Vayetzei", "Vayishlach",
+                "Vayeshev", "Channukah", "Miketz", "Vayigash", "Vaychi", "Shmos", "Vaera", "Bo", "Beshalach", "Yisro",
+                "Mishpatim", "Terumah", "Titzaveh", "Purim", "Ki Sisa", "Vayakel - Pekudei", "Vayakel", "Pekudei",
+                "Vayikra", "Tzav", "Shmini", "Tazria - Metzora", "Tazria", "Metzora", "Achrei Mos - Kedoshim",
+                "Achrei Mos", "Kedoshim", "Emor", "Behar - Bechukosai", "Behar", "Bechukosai", "Bamidbar and Shavuos",
+                "Bamidbar", "Shavuos", "Naso", "Behaaloscha", "Shlach", "Korach", "Chukas", "Balak", "Pinchas",
+                "Matos - Maasei", "Matos", "Maasei", "Devarim", "Vaeschanan", "Eikev", "Re'eh", "Shoftim",
+                "Ki Tzeitzei", "Ki Savo", "Nitzavim", "Vayeilech", "Haazinu and Succos", "Haazinu", "Vezot Haberachah"]
 
 
 def parse_filename(filename):
@@ -35,7 +41,10 @@ def get_pdfs():
             year, parsha = parse_filename(filename)
             if year and parsha:
                 pdfs.append((year, parsha, filename))
-    return sorted(pdfs, key=lambda x: (x[0], x[1]))
+
+    pdfs.sort(key=lambda x: (x[0], parsha_order.index(x[1]) if x[1] in parsha_order else float('inf')))
+
+    return pdfs
 
 
 def render_template(template_name, context):
@@ -52,7 +61,7 @@ def generate_html():
         grouped_pdfs[year].append((parsha, filename))
 
     index_html = render_template('index.html', {'grouped_pdfs': grouped_pdfs})
-    with open(os.path.join(OUTPUT_DIR, 'index.html'), 'w') as f:
+    with open('test.html', 'w') as f:
         f.write(index_html)
 
 
